60aa5893c83a1f32abf834d919f30e52ac779ac8
alexc <alexchoi357@hotmail.co.uk>
2020-08-03 03:35:03 -0700

    still working on it

--- src/test/model/AccountTest.java
+++ src/test/model/AccountTest.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.Test;
 class AccountTest {
     // delete or rename this class!
     private Account testAccount;
+    private Account testAccount2;
     private Stocks testStocks;
     private Stocks testStocks2;
 
     @BeforeEach
     void runBefore() {
-        testAccount = new Account("Alex",1000);
+        testAccount = new Account("Alex",1000.00);
+        testAccount2 = new Account("oof",0.00);
         testStocks = new Stocks("babo", 100.00);
         testStocks2 = new Stocks("yay", 5000.00);
         double testprevioustrans = 0;
@@ -23,40 +25,54 @@ class AccountTest {
     @Test
     void testConstructor() {
         assertEquals("Alex", testAccount.getName());
-        assertEquals(1000, testAccount.getBalance());
+        assertEquals(1000.00, testAccount.getBalance());
+    }
+
+    @Test
+    void testStock() {
+        assertEquals("babo",testStocks.getStockName());
+        assertEquals(100.00,testStocks.getStockWorth());
+    }
+
+    @Test
+    void testConstructorNegBalance() {
+        testAccount = new Account("Matt", -25.0);
+        assertEquals("Matt", testAccount.getName());
+        assertEquals(0, testAccount.getBalance());
+        assertTrue(testAccount.getId() > 0);
     }
 
     @Test
     void testDeposit() {
-        testAccount.deposit(5);
-        assertEquals(1005, testAccount.getBalance());
+        testAccount.deposit(5.00);
+        assertEquals(1005.00, testAccount.getBalance());
     }
 
     @Test
     void testWithdraw() {
-        testAccount.withdraw(105);
-        assertEquals(895, testAccount.getBalance());
+        testAccount.withdraw(105.00);
+        assertEquals(895.00, testAccount.getBalance());
     }
 
     @Test
     void testMultipleDeposits() {
-        testAccount.deposit(150);
-        testAccount.deposit(150);
-        assertEquals(1300, testAccount.getBalance());
+        testAccount.deposit(150.00);
+        testAccount.deposit(150.00);
+        assertEquals(1300.00, testAccount.getBalance());
     }
 
     @Test
     void testMultipleWithdrawals() {
-        testAccount.withdraw(150);
-        testAccount.withdraw(100);
-        assertEquals(750, testAccount.getBalance());
+        testAccount.withdraw(150.00);
+        testAccount.withdraw(100.00);
+        assertEquals(750.00, testAccount.getBalance());
     }
 
     @Test
     void testWithdrawDeposit() {
-        testAccount.withdraw(200);
-        testAccount.deposit(300);
-        assertEquals(1100, testAccount.getBalance());
+        testAccount.withdraw(200.00);
+        testAccount.deposit(300.00);
+        assertEquals(1100.00, testAccount.getBalance());
     }
 
     @Test
@@ -67,5 +83,40 @@ class AccountTest {
         assertEquals(5000.00, testStocks2.getStockWorth());
     }
 
+    @Test
+    void testgetId() {
+        assertEquals(7,testAccount.getId());
+        assertEquals(8,testAccount2.getId());
+    }
+
+    @Test
+    void testgetName() {
+        assertEquals("Alex",testAccount.getName());
+        assertEquals("oof",testAccount2.getName());
+    }
+
+    @Test
+    void testgetBalance() {
+        assertEquals(1000.00, testAccount.getBalance());
+        assertEquals(0.00,testAccount2.getBalance());
+    }
+
+    @Test
+    void testgetStockName() {
+        assertEquals("babo", testStocks.getStockName());
+        assertEquals("yay", testStocks2.getStockName());
+    }
+
+    @Test
+    void testgetStockWorth() {
+        assertEquals(100.00, testStocks.getStockWorth());
+        assertEquals(5000.00, testStocks2.getStockWorth());
+    }
+
+    @Test
+    void testToString() {
+        assertTrue(testAccount.toString().contains("name = Alex, balance = $1000.00"));
+    }
 
 }
+
